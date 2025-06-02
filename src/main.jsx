c413b96daaa3b08dd5a81f821e471899d04b8c55
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-02 20:51:32 +0300

    Create query client and add root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { root } from "./lib/loaders";
+import Root from "./pages/Root";
 import "./styles/main.css";
+
+const client = new QueryClient();
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <h1 className="text-9xl text-amber-300">Hello World</h1>,
+        element: <Root />,
+        loader: root(client),
     },
 ]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={client}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </StrictMode>,
 );
