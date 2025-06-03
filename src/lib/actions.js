914771f3f054b753c94a913880c15036fcc974ef
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-03 22:31:00 +0300

    Signup user

---
+++ src/lib/actions.js
@@ -0,0 +1,43 @@
+import { redirect } from "react-router";
+import jwt from "./jwt";
+import { getUser } from "./queries";
+
+export function signupUser(client) {
+    return async ({ request }) => {
+        const formData = await request.formData();
+
+        const response = await fetch(
+            `${import.meta.env.VITE_API_URL}/sign-up`,
+            {
+                method: "post",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    firstName: formData.get("firstName"),
+                    lastName: formData.get("lastName"),
+                    username: formData.get("username"),
+                    password: formData.get("password"),
+                    confirmPassword: formData.get("confirmPassword"),
+                }),
+            },
+        );
+
+        if (response.status === 400) {
+            return await response.json();
+        }
+
+        if (!response.ok) {
+            throw response;
+        }
+
+        const { token } = await response.json();
+
+        jwt.set(token);
+
+        client.removeQueries({ queryKey: getUser.queryKey });
+
+        return redirect("/");
+    };
+}
