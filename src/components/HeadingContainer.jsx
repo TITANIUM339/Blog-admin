5b45e4b4d0ac93e74c45ac8e51849b5bca22b31e
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-04 18:57:21 +0300

    Create HeadingContainer component

---
+++ src/components/HeadingContainer.jsx
@@ -0,0 +1,17 @@
+export default function HeadingContainer({ children }) {
+    return (
+        <div className="relative bg-teal-100">
+            {children}
+            <div className="absolute bottom-0 left-0 grid h-[60%] w-[30%] grid-rows-3">
+                <div className="w-[25%] origin-left animate-[scale_1s_ease-out] bg-teal-200"></div>
+                <div className="w-[50%] origin-left animate-[scale_1s_ease-out] bg-teal-400"></div>
+                <div className="w-full origin-left animate-[scale_1s_ease-out] bg-teal-600"></div>
+            </div>
+            <div className="absolute top-0 right-0 grid h-[60%] w-[30%] grid-rows-3 justify-items-end">
+                <div className="w-full origin-right animate-[scale_1s_ease-out] bg-teal-600"></div>
+                <div className="w-[50%] origin-right animate-[scale_1s_ease-out] bg-teal-400"></div>
+                <div className="w-[25%] origin-right animate-[scale_1s_ease-out] bg-teal-200"></div>
+            </div>
+        </div>
+    );
+}
