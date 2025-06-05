372ca68c41adcdb19e14feb7aef88a17370b0555
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-05 17:20:34 +0300

    Handle unexpected errors

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -8,8 +8,16 @@ export default function Error() {
         <div className="flex h-screen items-center justify-center">
             <section className="flex flex-col items-center gap-4">
                 <h1 className="text-3xl font-medium">
-                    <span className="text-teal-700">{error.status}:</span>{" "}
-                    {error.statusText}
+                    {error.status ? (
+                        <>
+                            <span className="text-teal-700">
+                                {error.status}:
+                            </span>{" "}
+                            {error.statusText}
+                        </>
+                    ) : (
+                        "An unexpected error occurred"
+                    )}
                 </h1>
                 <Button Component={Link} to="/" replace>
                     Go back home
