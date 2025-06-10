ec2c02926687efca81ba873c89610c7ebc2253a5
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-10 20:16:51 +0300

    Add z-index

--- src/components/HeadingContainer.jsx
+++ src/components/HeadingContainer.jsx
@@ -1,7 +1,7 @@
 export default function HeadingContainer({ children }) {
     return (
-        <div className="relative bg-teal-100">
-            {children}
+        <div className="relative bg-teal-50">
+            <div className="relative z-1">{children}</div>
             <div className="absolute bottom-0 left-0 grid h-[60%] w-[30%] grid-rows-3">
                 <div className="w-[25%] origin-left animate-[scale_1s_ease-out] bg-teal-200"></div>
                 <div className="w-[50%] origin-left animate-[scale_1s_ease-out] bg-teal-400"></div>
