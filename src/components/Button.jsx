06748d80863562214a3d88e2347a156fd7095231
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-20 20:40:15 +0300

    Add danger variant and remove className prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,5 @@
 export default function Button({
     variant = "primary",
-    className,
     Component,
     children,
     ...props
@@ -10,14 +9,15 @@ export default function Button({
             "rounded-md bg-teal-50 pt-1 pr-3 pb-1 pl-3 font-medium text-teal-700 transition-colors duration-200 hover:not-disabled:cursor-pointer hover:not-disabled:bg-teal-100",
         secondary:
             "rounded-md pt-1 pr-3 pb-1 pl-3 font-medium transition-colors duration-200 hover:not-disabled:cursor-pointer hover:not-disabled:bg-[rgba(0,0,0,0.1)]",
+        danger: "rounded-md pt-1 pr-3 pb-1 pl-3 font-medium text-red-700 transition-colors duration-200 hover:not-disabled:cursor-pointer hover:not-disabled:bg-[rgba(0,0,0,0.1)]",
     }[variant];
 
     return Component ? (
-        <Component className={`${variantClasses} ${className}`} {...props}>
+        <Component className={variantClasses} {...props}>
             {children}
         </Component>
     ) : (
-        <button className={`${variantClasses} ${className}`} {...props}>
+        <button className={variantClasses} {...props}>
             {children}
         </button>
     );
