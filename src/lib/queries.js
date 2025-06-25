01c51cc045716a0107b50c6003a88076b8c7b1de
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-25 18:31:51 +0300

    Create getPosts query

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,24 +1,58 @@
 import jwt from "./jwt";
 
-export const getUser = {
-    queryKey: ["user"],
-    async queryFn() {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/user`, {
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${jwt.get()}`,
-            },
-        });
+export function getUser() {
+    return {
+        queryKey: ["user"],
+        async queryFn() {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/user`,
+                {
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${jwt.get()}`,
+                    },
+                },
+            );
 
-        if (response.status === 401) {
-            return null;
-        }
+            if (response.status === 401) {
+                return null;
+            }
 
-        if (!response.ok) {
-            throw response;
-        }
+            if (!response.ok) {
+                throw response;
+            }
 
-        return await response.json();
-    },
-};
+            return await response.json();
+        },
+    };
+}
+
+export function getPosts(filter) {
+    return {
+        queryKey: ["user", "posts", filter],
+        async queryFn() {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/posts?myPosts=true${filter === "published" || filter === "drafts" ? `&published=${filter === "published"}` : ""}`,
+                {
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${jwt.get()}`,
+                    },
+                },
+            );
+
+            if (response.status === 401) {
+                return null;
+            }
+
+            if (!response.ok) {
+                throw response;
+            }
+
+            return await response.json();
+        },
+        staleTime: Infinity,
+    };
+}
