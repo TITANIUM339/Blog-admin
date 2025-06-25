bc72d86fa4c250dac8d5c27f715bc19fb187cc21
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-06-25 18:33:23 +0300

    Load user posts

--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -1,5 +1,12 @@
-import { getUser } from "./queries";
+import { getPosts, getUser } from "./queries";
 
 export function root(client) {
-    return async () => await client.ensureQueryData(getUser);
+    return async () => await client.ensureQueryData(getUser());
+}
+
+export function myPosts(client) {
+    return async ({ request }) =>
+        await client.fetchQuery(
+            getPosts(new URL(request.url).searchParams.get("filter") || "all"),
+        );
 }
