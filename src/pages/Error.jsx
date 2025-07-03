4745da90ecdad68fd0565a0db0d221a468ca0af8
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-07-03 10:43:02 +0300

    Refactor

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -13,23 +13,19 @@ export default function Error() {
 
     return (
         <div className="flex h-screen items-center justify-center">
-            <section className="flex flex-col items-center gap-4 text-center">
-                <h1 className="text-3xl font-medium">
-                    {error.status ? (
-                        <>
-                            <span className="text-teal-700">
-                                {error.status}:
-                            </span>{" "}
-                            {error.statusText}
-                        </>
-                    ) : (
-                        "An unexpected error occurred"
-                    )}
-                </h1>
+            <div className="flex flex-col items-center gap-4 text-center">
+                <section>
+                    <h1 className="text-3xl font-medium text-teal-700">
+                        {error.status
+                            ? error.status
+                            : "An unexpected error occurred"}
+                    </h1>
+                    <p className="text-lg">{error.statusText || error.data}</p>
+                </section>
                 <Button Component={Link} to="/" replace>
                     Go back home
                 </Button>
-            </section>
+            </div>
         </div>
     );
 }
